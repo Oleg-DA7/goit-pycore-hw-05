37f21c853cecdcbc5ecf6c735136cfdd948d9550
Shon <onshad@gmail.com>
2025-03-03 20:34:19 +0200

    v1

--- task 1.py
+++ task 1.py
@@ -4,11 +4,11 @@
 def caching_fibonacci():
     cache = {}
     def fibonacci(n):
-        if n in cache:
+        if n in cache:              # if already calculated 
             return cache[n]
-        if n <= 0:
+        if n <= 0:                  # 1st element
             return 0
-        if n == 1: 
+        if n == 1:                  # 2nd element
             return 1
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         print(f'{n} - {cache[n]}')
@@ -16,5 +16,5 @@ def caching_fibonacci():
     return fibonacci
 
 f1 = caching_fibonacci()
-print(f1(15))
+print(f1(5))
 
