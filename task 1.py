ccf9587e17fdabc0bac1aa0505634733fd2848c8
Shon <onshad@gmail.com>
2025-03-03 20:28:42 +0200

    v1

--- task 1.py
+++ task 1.py
@@ -1,1 +1,20 @@
-# lets do it
+# Завдання 1
+# Ряд Фібоначчі
+
+def caching_fibonacci():
+    cache = {}
+    def fibonacci(n):
+        if n in cache:
+            return cache[n]
+        if n <= 0:
+            return 0
+        if n == 1: 
+            return 1
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        print(f'{n} - {cache[n]}')
+        return cache[n]
+    return fibonacci
+
+f1 = caching_fibonacci()
+print(f1(15))
+
