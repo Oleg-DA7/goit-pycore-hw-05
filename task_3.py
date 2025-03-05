01eb469a10395f9685b091310b9c3f5de4437cf8
Shon <onshad@gmail.com>
2025-03-05 22:42:50 +0200

    v3

--- task_3.py
+++ task_3.py
@@ -1,35 +1,62 @@
 # Завдання 3
 # 
 import sys
-
+import pandas as pd
 
 class LogEngine:
     def __init__(self, file):
         self.file = file
-        self._load_logs(file)
-        
-    def _parse_log_line(line: str) -> dict:
-        pass    
+        self._df_log = pd.DataFrame(columns=['date', 'time', 'type', 'message'])
+        self._loaded = self._load_logs(file)
+        if self._loaded:
+            self.count_logs_by_level()
+            self.display_log_counts()
+        else:
+            sys.exit
 
-    def _load_logs(file_path: str) -> list:
-        pass
+    def count_logs_by_level(self) -> dict: 
+        self._df_stat = self._df_log.groupby('type')['message'].count()
+        return self._df_stat.to_list()
 
-    def filter_logs_by_level(logs: list, level: str) -> list:
-        pass
+    def _parse_log_line(self, line: str) -> dict:
+        result = line.replace('\n', '').split(' ', 3)        
+        return result
 
-    def count_logs_by_level(logs: list) -> dict: 
-        pass
+    def _load_logs(self, file_path: str) -> list:
+        try:
+            with open(file_path) as file:    
+                for line in file: 
+                    l_list = self._parse_log_line(line)                
+                    self._df_log.loc[len(self._df_log)] = l_list
+        except FileNotFoundError:
+            print(f'Can`t find file {file_path}')
+            return False
+        except IOError:
+            print(f'An error occurred while reading the file {file_path}')
+            return False
+        return True
 
-    def display_log_counts(counts: dict):
-        pass
+    def filter_logs_by_level(self, level: str) -> list:
+        result = []
+        df_filtered = self._df_log[self._df_log['type'] == level]
+        for i, r in df_filtered.iterrows():
+            print(f'{r['date']} {r['time']} - {r['message']}')
+            result.append(f'{r['date']} {r['time']} - {r['message']}')
+        return result
+
+    def display_log_counts(self):
+        print('Рівень логування | Кількість')
+        print('-----------------|----------')
+        for i, v in self._df_stat.items():
+            print(f'{'{: <17}'.format(i)}|  {v}')
     
 def main():
-    le = LogEngine()
-    if len(sys.argv) > 2:
+    if len(sys.argv) < 2 | len(sys.argv) > 3:
         print("Invalid count of parameters!")
         sys.exit(1)
-
-
+    le = LogEngine(sys.argv[1])
+    if len(sys.argv) == 3:
+        le.filter_logs_by_level(sys.argv[2])
 
 if __name__ == "__main__":
     main()
