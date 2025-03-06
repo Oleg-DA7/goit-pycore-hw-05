bdb3ee2aaf2d93296a5c0dc5c03392c1de4c29db
Shon <onshad@gmail.com>
2025-03-06 21:42:33 +0200

    v4

--- task_4.py
+++ task_4.py
@@ -1,41 +1,55 @@
 from functools import wraps
 
+def error_decorator(default_result=None):
+    def decorator(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except ValueError:
+                print("Give me name and phone please.")
+                return default_result                
+            except (KeyError, IndexError, TypeError):            
+                print("Enter the argument for the command")
+                return default_result
+        return inner
+    return decorator
 
-def input_error(func):
-    @wraps(func)
-    def inner(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except ValueError:
-            return "Give me name and phone please."
-        except (KeyError, IndexError):
-            print("Enter the argument for the command")
-    return inner
-
+@error_decorator(default_result=None)
 class Contact():
-    def __init__(self, name, phone):
-        self.name = name
-        self.phone = phone 
+    def __init__(self, args):
+        self.name = args[0]
+        self.phone = args[1]
     def __str__(self):
         return f'Contact name: {self.name}, phone: {self.phone}'
+
 
 class ContactList():
     def __init__(self):
         self.contacts = []    
+
+    @error_decorator(default_result=None)
     def add_contact(self, contact):
         self.contacts.append(contact)
         return (f'{contact} added.')
     def all_contacts(self):
         for i in self.contacts: 
             print(f'{i}')
-    def get_phone(self, name):
+
+    @error_decorator(default_result=None)
+    def get_phone(self, args):
+        name = args[0]
         result = ''
         for i in self.contacts:
             if i.name == name: 
                 result += f'Contact {i.name} phone is {i.phone} \n'
         if result == '': result = 'Phone not found'
         return result
-    def change_contact(self, name, phone):
+    
+    @error_decorator(default_result=None)
+    def change_contact(self, args):
+        name = args[0]
+        phone = args[1]
         result = 'Phone not found'
         for i in self.contacts:
             if i.name == name: 
@@ -43,21 +57,18 @@ class ContactList():
                 result = f'Contact updated to {i}'
         return result
 
-def parse_input(user_input):
+@error_decorator(default_result=[None, None])
+def parse_input(user_input):    
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
-@input_error
 def main():
     contacts = ContactList()
     print("Welcome to the assistant bot!")
     while True:
-#        try:
          user_input = input("Enter a command: ")
          command, *args = parse_input(user_input)
-#        except Exception as ex:
-#            pass
          match command:
             case "close" | "exit":
                 print("Good bye!")
@@ -65,16 +76,19 @@ def main():
             case "hello":
                 print("How can I help you?")
             case "add":
-                contact = Contact(args[0], args[1])
-                print(contacts.add_contact(contact))
-            case "change":                
-                print(contacts.change_contact(args[0], args[1]))
+                contact = Contact(args)
+                if contact != None:
+                    print(contacts.add_contact(contact))                                  
+            case "change":
+                result = contacts.change_contact(args)                 
+                if result != None:
+                    print(result)
             case "phone":
-                print(contacts.get_phone(args[0]))
+                result = contacts.get_phone(args)
+                if result != None:
+                    print(result)
             case "all":
                 contacts.all_contacts()
-            case _:
-                print("Invalid command.")
 
 
 if __name__ == "__main__":
